3852527a106b2fe3a17451be2475f36b01145258
matrupihai <matrupihai@gmail.com>
2015-01-03 18:33:35 +0200

    cannot connect

--- WebLib/src/com/weblib/hbm/model/Author.java
+++ WebLib/src/com/weblib/hbm/model/Author.java
@@ -2,13 +2,17 @@ package com.weblib.hbm.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity (name="100_AUTHORS")
 public class Author {
 	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Column (name="author_id")
 	private int authorId;
+	
 	@Column (name="author_name")
 	private String authorName;
 	
--- WebLib/src/com/weblib/main/WebLib.java
+++ WebLib/src/com/weblib/main/WebLib.java
@@ -1,12 +1,24 @@
 package com.weblib.main;
 
-import com.weblib.dao.TestDAO;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.weblib.hbm.util.HibernateUtil;
+
 
 
 public class WebLib {
 	
 	public static void main(String[] args) {
-		System.out.println(new TestDAO().getTestObjects());
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Author");
+		List list = query.list(); 
+		session.close();
+		
+		System.out.println(list);
 	}
 	
 }
