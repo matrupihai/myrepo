7f03d378e0162baec090309f72b47bc3f4b2b055
matrupihai <matrupihai@gmail.com>
2015-01-03 15:58:54 +0200

    -

--- WebLib/src/com/weblib/hbm/model/Author.java
+++ WebLib/src/com/weblib/hbm/model/Author.java
@@ -1,12 +1,15 @@
 package com.weblib.hbm.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity (name="100_AUTHORS")
 public class Author {
 	@Id
+	@Column (name="author_id")
 	private int authorId;
+	@Column (name="author_name")
 	private String authorName;
 	
 	public int getAuthorId() {
