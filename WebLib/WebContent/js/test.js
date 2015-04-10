75899434fa6f625962eb92e08e9ede4cc3373933
matrupihai <matrupihai@gmail.com>
2015-04-11 02:32:11 +0300

    book detail card mockup

--- WebLib/WebContent/js/test.js
+++ WebLib/WebContent/js/test.js
@@ -1,26 +1,26 @@
-var url = "http://localhost:8080/WebLib/authors";
+var booksUrl = "http://localhost:8080/WebLib/books";
 
 $("document").ready(
-	findAll()
+	findAllBooks()
 );
 
-function findAll() {
+function findAllBooks() {
 	console.log("findAll");
 	$.ajax({
 		type: "GET",
-		url: url,
+		url: booksUrl,
 		dataType: "json", 
 		success: renderList
 	});
 }
 
-function renderList(data) {
+function renderBooksList(data) {
 	var newData = data == null ? [] : (data instanceof Array ? data : [data]);
 	var list = '';
 	$.each(newData, function(i, author) {
 		list += "<li>" + author.authorName + "</li>";
 	});
-	$("#authorsList").append(list);
+	$("document").append(list);
 }
 
 
