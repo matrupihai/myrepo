c4aa4aa076ffb758ec694c7896fcb48f34549744
matrupihai <matrupihai@gmail.com>
2015-04-15 01:05:37 +0300

    +

---
+++ WebLib/src/com/weblib/hbm/util/ResponseFilter.java
@@ -0,0 +1,17 @@
+package com.weblib.hbm.util;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+
+public class ResponseFilter implements ContainerResponseFilter {
+
+	@Override
+	public ContainerResponse filter(ContainerRequest request,
+			ContainerResponse response) {
+		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+		
+		return response;
+	}
+}
