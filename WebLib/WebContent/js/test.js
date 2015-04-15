16512249c75a05367e9565c37504a8119b259470
matrupihai <matrupihai@gmail.com>
2015-04-15 14:20:23 +0300

    json associated to div model

--- WebLib/WebContent/js/test.js
+++ WebLib/WebContent/js/test.js
@@ -21,60 +21,63 @@ function findAllBooks() {
 function renderBooksList(data) {
 	var newData = data == null ? [] : (data instanceof Array ? data : [data]);
 	$.each(newData, function(i, book) {
-		$(".booksContainer").append(renderBook(book));
+		var bookCard = renderBook(book);
+		var jsonBook = JSON.stringify(book);
+		jQuery.data(bookCard[0], "bookModel", jsonBook);
+		$(".booksContainer").append(bookCard);
 	});
 }
 
 function renderBook(book) {
 	var bookAuthors = [];
-	
+
 	$.each(book.authors, function(i, author) {
 		bookAuthors.push(author.authorName);
 	});
-	
+
 	var bookCard = $("<div/>", {
 		class: "bookCard"
 	});
-	
+
 	var bookHeader = $("<div/>", {
 		class: "bookHeader"
 	});
-	
+
 	var bookTitle = $("<h1/>", {
 		class: "bookTitle",
 		html: book.title
 	});
-	
+
 	var bookAuthor = $("<p/>", {
 		class: "bookAuthor",
 		html: bookAuthors.toString()
 	});
-	
+
 	var bookPages = $("<span/>", {
 		class: "bookPages",
 		html: book.noOfPages + " pages"
 	});
-	
+
 	var bookImg = $("<div/>", {
 		class: "bookImg",
-		html: "<img alt=\"bookImg\" src=\"img/imgStub.png\" />"
+		html: "<img alt=\"bookImg\" src=\"img/imgStub.png\" />",
 	});
-	
+
 	var bookDescription = $("<p/>", {
 		class: "bookDescription",
 		html: loremIpsum
 	});
-	
+
 	var bookActions = $("<div/>", {
 		class: "bookActions"
 	});
-	
-	var borrowBook = $("<a/>", {
-		id: "borrowBook",
-		href: "#",
+
+	var borrowBook = $("<button/>", {
+		class: "borrowBook",
+		type: "button",	
 		html: "Borrow"
 	});
-	
+
 	bookImg.appendTo(bookCard);
 	bookHeader.appendTo(bookCard);
 	bookTitle.appendTo(bookHeader);
@@ -83,24 +86,14 @@ function renderBook(book) {
 	bookDescription.appendTo(bookCard);
 	bookActions.appendTo(bookCard);
 	borrowBook.appendTo(bookActions);
-	jQuery.data(bookCard, book);
-	
+
 	return bookCard;
 }
 
-$("document").ready(function() {
-	$("#borrowBook").click(function(event) {
-		alert("Hello!");
-		var book = $(this).closest(".bookCard").data(); 
-		alert(book.yearPublished);
-	});
+$(document).on("click", ".borrowBook", function(event) {
+	var bookCard = $(event.target).closest(".bookCard"); 
+	var bookJson = JSON.parse(jQuery.data(bookCard[0], "bookModel")); 
+	console.log(bookJson.yearPublished);
 });
-
-
-
-
-
-
-
 
 
